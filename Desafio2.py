c3e4f2997f38331fb75dca10d1f8890131dc3bac
mariafedechen-tech <maria.fedechen@escola.pr.gov.br>
2026-03-18 08:44:00 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,1 +1,8 @@
-
+# Solicita que o usuário digite um número
+numero = int(input("Digite um número: "))
+# Verifique se o número digitado é par
+if numero % 2 == 0:
+  # Dê a resposta
+     print('número par')
+else: 
+     print('número ímpar')
