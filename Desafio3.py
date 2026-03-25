206fea8cb817301e1ba57d594c622bf34ab9b940
mariafedechen-tech <maria.fedechen@escola.pr.gov.br>
2026-03-25 14:42:58 -0300

    Update Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,6 +1,17 @@
-#criar uma lista com 5 nomes
-nomes = ["Mayelli", "Parisotto", "Rodrigo", "Maria", "Marciani"]
+# Código de lista de 5 nomes
+# Será pedido ao usuário para escrever 5 nomes de sua escolha
+print("Escreva 5 nomes:")
 
-# Vai ser apresentado em forma de lista os 5 nomes digitados pelo usuário
-for nome in nomes:
-    print(nome)
+nome1 = input("Digite o nome 1: ")
+nome2 = input("Digite o nome 2: ")
+nome3 = input("Digite o nome 3: ")
+nome4 = input("Digite o nome 4: ")
+nome5 = input("Digite o nome 5: ")
+
+# Vai ser exibido em forma de lista, de "tópicos" os nomes escolhidos
+print("A lista com os nomes escritos ´é:")
+print(nome1)
+print(nome2)
+print(nome3)
+print(nome4)
+print(nome5)
