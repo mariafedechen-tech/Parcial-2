8a6cae3e5daa625521cd8cedcc5416dc16bad5f5
mariafedechen-tech <maria.fedechen@escola.pr.gov.br>
2026-03-25 14:53:01 -0300

    Update Desafio6.py

--- Desafio6.py
+++ Desafio6.py
@@ -1,14 +1,26 @@
 # Código que converte um número de segundos para horas, minutos e segundos
 # Outro código depois que irá fazer o contrário
-segundos = int(input("escreva a quantidade de segundos"))
+segundos = int(input("escreva a quantidade de segundos: "))
 minutos  = (segundos//60)
 horas    = (segundos//3600)
 
-print("esse número de segundos tem como resultado", horas, "horas","ou" minutos, "minutos e ainda", segundos, "segundos")
+# Será mostrado o resultado
+print("horas:")
+print(horas)
+print("minutos:")
+print(minutos)
+print("segundos:")
+print(segundos)
 
 # Será feito o contrário agora, para isso será pedido para o usuário escrever uma quantidade de horas
-h = int(input( "escreva quantas horas"))
+h = int(input( "escreva quantas horas: "))
 seg = ( h * 3600 )
 m = ( h * 60 ) 
-print("esse número de horas tem como resultado", h, "horas", "ou", m, "minutos e ainda", seg, "segundos")
+# Será mostrado o resultado
+print("horas:")
+print(h)
+print("minutos:")
+print(m)
+print("segundos:")
+print(seg)
    
