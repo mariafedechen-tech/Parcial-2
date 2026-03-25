c0181eb70e32112ffedde93c0f7c30df31cca7d4
mariafedechen-tech <maria.fedechen@escola.pr.gov.br>
2026-03-25 08:06:56 -0300

    Update Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,6 +1,6 @@
-#criar lista com 5 nomes
+#criar uma lista com 5 nomes
 nomes = ["Mayelli", "Parisotto", "Rodrigo", "Maria", "Marciani"]
 
-# Imprimir os nomes da lista
+# Vai ser apresentado em forma de lista os 5 nomes digitados pelo usuário
 for nome in nomes:
     print(nome)
