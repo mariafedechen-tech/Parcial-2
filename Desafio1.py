d5ec64b0323e425ea75b2b5886db326c22e7f855
mariafedechen-tech <maria.fedechen@escola.pr.gov.br>
2026-03-25 14:56:08 -0300

    Update Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,6 +1,7 @@
+# Código para fazer a soma de dois números
 # Será solicitado para o usuário que ele digite os dois números 
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
+numero1 = float(input("Escreva o primeiro número: "))
+numero2 = float(input("Escreva o segundo número: "))
 
 # Será calculada a soma dos dois números que o usuário escreveu
 soma = numero1 + numero2
