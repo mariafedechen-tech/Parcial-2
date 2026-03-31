709382206434cc7d4ed02442fceaaffcf0c87af0
mariafedechen-tech <maria.fedechen@escola.pr.gov.br>
2026-03-31 09:28:56 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,11 +1,13 @@
 # Código para verificar se um número é par
 # Vai ser pedido para o usuário digitar algum número
 # O int é usado para números inteiros
-# o input é usado para 
+# o input é usado para interpretar os números e no geral o que a pessoa digitar 
 numero = int(input("Digite um número: "))
 # Será verificado se o número digitado pelo usuário é par
+# O if é usado para dar uma condição, para caso a afirmação seja verdade
 if numero % 2 == 0:
   # A resposta vai ser apresentada
-     print('número par')
+     print('número par') 
+  # O else é usado para quando de acordo com o if a afirmação for falsa
 else: 
      print('número ímpar')
